52986c89669ede16c8822c5283b073a7e508a5b4
Nolan Wira <njw1389@rit.edu>
2024-09-11 13:21:56 -0400

    Exercise 1B - Version Control - Part 2 Commit

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 echo "Cleaning existing classes..."
-rm -f *.class
-# This command looks for matching files and runs the rm command for each file
-# Note that {} are replaced with each file name
-find . -name \*.class -exec rm {} \;
+rm -rf build
+mkdir -p build
+# Clean up any stray .class files in the main directory
+find . -maxdepth 1 -name "*.class" -type f -delete
 
 echo "Compiling source code and unit tests..."
-javac -classpath .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar *.java
+javac -d build -classpath .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar src/main/java/*.java src/test/java/*.java
 if [ $? -ne 0 ] ; then echo BUILD FAILED!; exit 1; fi
 
 echo "Running unit tests..."
-java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
+java -cp build:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
 if [ $? -ne 0 ] ; then echo TESTS FAILED!; exit 1; fi
 
 echo "Running application..."
-java -classpath . RunEdgeConvert
+java -cp build RunEdgeConvert
